6734af34862697bb372a28085843dfae344875dd
Ian Fang <xfang@uwf.edu>
2022-02-07 10:11:16 -0600

    update

--- ch03-modular/main.cpp
+++ ch03-modular/main.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
 #include "threeintsfcts.hpp"
+#include <iostream>
+
 using namespace std;
 
 // Normally lots of other code here
 
-int main()
-{
+int main() {
 
+  cout << "Testing three int sum and three int avg function " <<
+      "on 5, 10, 20" << endl;
   cout << ThreeIntsSum(5, 10, 20) << endl;
   cout << ThreeIntsAvg(5, 10, 20) << endl;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
