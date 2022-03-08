707ee006d70bb914f02d62e977077146e5a9b14a
Ian Fang <xfang@uwf.edu>
2022-03-07 23:27:33 -0600

    update sorting

--- ch07-sorting-string/sort.cpp
+++ ch07-sorting-string/sort.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <climits>
+#include <cassert>
+
 using namespace std;
 
+// Utilities
 void printAll(int arr[], int size);
+
+// sorting functions
 void insertSort(int arr[], int size);
-int minLoc(int arr[], int size, int start);
+
+// Find the index of the minimum value
+// with start and end index specified
+int minLoc(int arr[], int start, int end);
 void selectSort(int arr[], int size);
 
+// testing functions
 void testMinLoc();
 void testInsertSort();
 void testSelectSort();
 
 void insertSort(int arr[], int size) {
-  for (int i = 1; i < size; i++) {
+  for (int i = 1; i < size; i++)
     for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+      // may use swap(arr[j], arr[j - 1]);
       int temp = arr[j];
       arr[j] = arr[j - 1];
       arr[j - 1] = temp;
     }
-  }
 }
 
-int minLoc(int arr[], int size, int start) {
-  int currMin = -1;
+int minLoc(int arr[], int start, int end) {
+  // Method 1: set currMin to INT_MAX and start from index 0
+  // Method 2: set currMin to arr[0] and start from index 1, must check if the
+  //     array is empty
+  int currMin = INT_MAX;
   int location = 0;
-  for (int i = start; i < size; i++) {
-    if (currMin == -1 || arr[i] < currMin) {
+  for (int i = start; i <= end; i++)
+    if (arr[i] < currMin) {
       currMin = arr[i];
       location = i;
     }
-  }
   return location;
 }
 
@@ -37,8 +50,8 @@ void selectSort(int arr[], int size) {
   // iterate through the index you are going swap with the minimal
   for (int index = 0; index <= size - 2; index++) {
     // find the location of the minimal value
-    int location = minLoc(arr, size, index);
-    // swap
+    int location = minLoc(arr, index, size - 1);
+    // may use swap(arr[index], arr[location])
     int temp = arr[index];
     arr[index] = arr[location];
     arr[location] = temp;
@@ -53,17 +66,22 @@ void printAll(int arr[], int size) {
 
 void testMinLoc() {
   int size = 5;
-  int arr[5] = {4, 5, 2, 1, 3};
+  int *arr = new int[size]{4, 5, 2, 1, 3};
 
-  cout << minLoc(arr, size, 2) << endl;
+  cout << "Testing minLoc function\n";
+  assert(minLoc(arr, 0, size - 1) == 3);
+  cout << "Tests passed\n";
+  delete [] arr;
 }
 
 void testSelectSort() {
   int size = 5;
   int arr[5] = {4, 5, 2, 1, 3};
 
+  cout << "Before Sorting: ";
   printAll(arr, size);
   selectSort(arr, size);
+  cout << "After Selection Sort: ";
   printAll(arr, size);
 }
 
@@ -71,15 +89,17 @@ void testInsertSort() {
   int size = 5;
   int arr[5] = {4, 5, 2, 1, 3};
 
+  cout << "Before Sorting: ";
   printAll(arr, size);
   insertSort(arr, size);
+  cout << "After Insertion Sort: ";
   printAll(arr, size);
 }
 
 
 int main() {
-  // testMinLoc();
-  // testSelectSort();
+  testMinLoc();
+  testSelectSort();
   testInsertSort();
   return EXIT_SUCCESS;
 }
